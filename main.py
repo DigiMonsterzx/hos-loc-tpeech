459cc19eb39e0a105a5a6b14e41db51074ff6794
DigiMonsterzx <mister.nour22@gmail.com>
2024-08-15 20:33:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from telegram import Update
-from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 import os
 import requests
 import cloudinary
